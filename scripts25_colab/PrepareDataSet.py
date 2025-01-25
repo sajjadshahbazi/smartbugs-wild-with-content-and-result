ab897e2745b4f8eeb30a58a04d40ff7d4456cff0
Sajjad <shahbazi.developer@gmail.com>
2025-01-26 00:02:45 +0330

    comment prepare data

--- scripts25_colab/PrepareDataSet.py
+++ scripts25_colab/PrepareDataSet.py
@@ -630,12 +630,12 @@ def train_unet_lstm():
     print("Model training completed and saved.")
 
 if __name__ == "__main__":
-    files = [os.path.join(PATH, f) for f in os.listdir(PATH) if f.endswith(".sol")]
-    print(f"size files {files.__len__()}")
-    for batch_index, i in enumerate(range(0, len(files), batch_size)):
-        batch_files = files[i:i + batch_size]
-        print(f"size batch_files {batch_files.__len__()}")
-        process_batch_with_categorization(batch_files, target_vulner, batch_size, batch_index)
+    # files = [os.path.join(PATH, f) for f in os.listdir(PATH) if f.endswith(".sol")]
+    # print(f"size files {files.__len__()}")
+    # for batch_index, i in enumerate(range(0, len(files), batch_size)):
+    #     batch_files = files[i:i + batch_size]
+    #     print(f"size batch_files {batch_files.__len__()}")
+    #     process_batch_with_categorization(batch_files, target_vulner, batch_size, batch_index)
 
 
     train_unet_lstm()
