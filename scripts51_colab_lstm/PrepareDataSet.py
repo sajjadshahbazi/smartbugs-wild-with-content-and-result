c2d3ab18e3b32cb88ea715d7a7b329a01b8eb3e8
Sajjad <shahbazi.developer@gmail.com>
2025-11-28 02:24:44 +0330

    simple 51 02

--- scripts51_colab_lstm/PrepareDataSet.py
+++ scripts51_colab_lstm/PrepareDataSet.py
@@ -57,7 +57,7 @@ target_vulner = target_vulnerability_reentrancy
 # CACHE_DIR = os.path.join(ROOT, 'vectorcollections')
 
 ROOT = '/content/smartbugs-wild-with-content-and-result' # Linux
-CACHE_DIR = os.path.join(ROOT, 'vectorcollections') # Linux
+CACHE_DIR = os.path.join(ROOT, 'vectorcollections02') # Linux
 
 cache_path = os.path.join(CACHE_DIR, 'tokenized_fragments.pkl')
 vulnerability_fd = open(os.path.join(ROOT, 'metadata', 'vulnerabilities.csv'), 'w', encoding='utf-8')
